6e827e5729da97e07583a316858b5ae774b6e467
dhilipsiva <dhilipsiva@pm.me>
2021-05-23 12:36:39 +0530

    display question list and detail

--- src/routes/questions/detail.js
+++ src/routes/questions/detail.js
@@ -1,13 +1,48 @@
 import React, { useState } from "react";
-import { useParams } from "react-router-dom";
+import graphql from "babel-plugin-relay/macro";
+import { Link, useRouteMatch, useParams } from "react-router-dom";
 
+const { useLazyLoadQuery } = require("react-relay");
+
+const query = graphql`
+  query detailQuestionQuery($id: ID!) {
+    question(id: $id) {
+      id
+      text
+      title
+      voteCount
+      commentCount
+      answers {
+        edges {
+          node {
+            id
+            text
+            voteCount
+          }
+        }
+      }
+    }
+  }
+`;
 function Detail() {
   let { questionId } = useParams();
-  return (
-    <div>
-      <p>Body: {questionId}</p>
-    </div>
-  );
+  const data = useLazyLoadQuery(query, { id: questionId });
+  if (data && data.question) {
+    return (
+      <div>
+        <p>{data.question.text}</p>
+        <ul>
+          {data.question.answers.edges.map((answerEdge) => (
+            <div>
+              <li>{answerEdge.node.text}</li>
+            </div>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return <p>Loading</p>;
+  }
 }
 
 export default Detail;
--- src/routes/questions/list.js
+++ src/routes/questions/list.js
@@ -1,10 +1,34 @@
 import React, { useState } from "react";
+import graphql from "babel-plugin-relay/macro";
+import { Link, useRouteMatch } from "react-router-dom";
+
+const { useLazyLoadQuery } = require("react-relay");
+
+const query = graphql`
+  query listQuestionsQuery {
+    questions {
+      id
+      text
+      title
+      voteCount
+      commentCount
+    }
+  }
+`;
 
 function List() {
+  const data = useLazyLoadQuery(query);
+  const { url } = useRouteMatch();
   return (
-    <div>
-      <p>List</p>
-    </div>
+    <ul>
+      {data &&
+        data.questions &&
+        data.questions.map((question) => (
+          <li>
+            <Link to={`${url}/${question.id}`}>{question.title}</Link>
+          </li>
+        ))}
+    </ul>
   );
 }
 
